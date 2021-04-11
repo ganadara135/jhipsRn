5bb2289b14664271441f3aeca96f7598e24e5c29
ganadara135 <ganadara135@gmail.com>
2021-04-11 17:30:07 +0900

    Initial version of org generated by JHipster-7.0.1

---
+++ src/test/java/y/IntegrationTest.java
@@ -0,0 +1,20 @@
+package y;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import y.OrgApp;
+import y.RedisTestContainerExtension;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = OrgApp.class)
+@ExtendWith(RedisTestContainerExtension.class)
+public @interface IntegrationTest {
+}
